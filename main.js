41bacaa7507384289bba278006396ed0cae5a74b
silviaespanagil <silviaespanagil@gmail.com>
2021-05-12 10:30:18 +0200

    Add and test funcion numberVerification

--- main.js
+++ main.js
@@ -2,26 +2,26 @@
 //validacion numero si el numero es mayor o igual a 1 o menor o igual a 100
 const button = document.querySelector(".js-button");
 const userNumber = document.querySelector(".js-userNumber");
-const clue = document.querySelector("js-clue");
+const clue = document.querySelector(".js-clue");
+const counter = document.querySelector(".js-counter");
 const randomNumber = getRandomNumber(100);
 
-/*function numberVerification() {
+function numberVerification() {
   const number = parseInt(userNumber.value);
-  if (number > 100 || number < 1);
-  {
+  if (number > 100 || number < 1) {
     clue.innerHTML = "El número debe estar entre 1 y 100";
-  }
-  /*else if (number < randomNumber)
-  {
+  } else if (number < randomNumber) {
     clue.innerHTML = "Demasiado bajo";
-  }*/
-}*/
+  } else if (number > randomNumber) {
+    clue.innerHTML = "Demasiado alto";
+  } else if (number === randomNumber) {
+    clue.innerHTML = "Has ganado campeona!!!";
+  }
+}
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-function handlerClick (){
-  counter ();
 
 window.addEventListener("load", getRandomNumber);
 button.addEventListener("click", numberVerification);
