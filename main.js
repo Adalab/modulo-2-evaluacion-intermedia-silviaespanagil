2f456e5d449628c12438cc6cdeb824fe39a2d0be
silviaespanagil <silviaespanagil@gmail.com>
2021-05-12 09:43:02 +0200

    Add and test randomNumber generator on window load

--- main.js
+++ main.js
@@ -1,33 +1,11 @@
 "use strict";
 
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const reset = document.querySelector(".reset");
-const totalPears = document.querySelector(".totalPears");
-const basket = {};
-basket.max = 10;
-basket.min = 0;
-basket.actual = 0;
-basket.initial = 1;
+//para el evento load clase body .js-body
 
-basket.addPear = function (a) {
-  if (basket.actual < basket.max) {
-    basket.actual += 1;
-  }
-  totalPears.innerHTML = basket.actual;
-};
-basket.restPear = function (a) {
-  if (basket.actual > basket.min) {
-    basket.actual -= 1;
-  }
+const button = document.querySelector(".js-button");
 
-  totalPears.innerHTML = basket.actual;
-};
-basket.resetPear = function (a) {
-  a = basket.initial;
-
-  totalPears.innerHTML = a;
-};
-plus.addEventListener("click", basket.addPear);
-minus.addEventListener("click", basket.restPear);
-reset.addEventListener("click", basket.resetPear);
+function getRandomNumber() {
+  const randomNumber = Math.ceil(Math.random() * 100);
+  return console.log(randomNumber);
+}
+window.addEventListener("load", getRandomNumber);
